1d2913919fc4d06c1b800c9f8e262d82685dbcb9
xavfa <xavierf@kth.se>
2021-10-27 14:52:26 +0200

    with mod(7) correction for the day ofthe week and weeks ends

--- CoreFiles/ProbGenerator.py
+++ CoreFiles/ProbGenerator.py
@@ -50,8 +50,8 @@ def BuildTempSetPoints(name,path,Values,Hours):
     FirstDayofWeek = 0 #the first day is a Monday
     for i in range(8760):
         HrperDay = i%24                 #this will gives us values between 0 and 24hours
-        DayofWeek = int(i % (24 * 7) / 24) + FirstDayofWeek # this will give the day of the week (0 is for Mondays)
-        WE = False if DayofWeek < 6 else True  #this will gove a 1 when we are on weekends
+        DayofWeek = (int(i % (24 * 7) / 24) + FirstDayofWeek)%7 # this will give the day of the week (0 is for Mondays)
+        WE = False if DayofWeek < 5 else True  #this will gove a 1 when we are on weekends
         if HrperDay<int(Hours[0][:Hours[0].find(':')]) or HrperDay>int(Hours[1][:Hours[1].find(':')]):# or WE:
             SetPoint.append(float(Values[1]))
         else:
@@ -64,8 +64,8 @@ def BuildOccupancyFile(name,path,min,max,building):
     FirstDayofWeek = 0 #the foirst day is a Monday
     for i in range(8760):
         HrperDay = i%24                 #this will gives us values between 0 and 24hours
-        DayofWeek = int(i % (24 * 7) / 24) + FirstDayofWeek # this will give the day of the week (0 is for Mondays)
-        WE = False if DayofWeek < 6 else True  #this will gove a 1 when we are on weekends
+        DayofWeek = (int(i % (24 * 7) / 24) + FirstDayofWeek)%7 # this will give the day of the week (0 is for Mondays)
+        WE = False if DayofWeek < 5 else True  #this will gove a 1 when we are on weekends
         if HrperDay<int(building.Office_Open[:building.Office_Open.find(':')]) or HrperDay>int(building.Office_Close[:building.Office_Close.find(':')]):# or WE:
             nbocc.append(0)
         else:
