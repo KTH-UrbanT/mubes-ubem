1e3ec403389c7b9b66076deff6d149a78b163b77
Hossein <mou.alizade@gmail.com>
2025-01-23 16:56:54 +0100

    ECM folder containing retrofitting materials added

---
+++ bin/Enegy_Conservation_Measure/HVACSystems.py
@@ -0,0 +1,26 @@
+def ZoneCtrl(idf,zone,building,PeopleDensity,ThermostatName, Multiplier,Correctdeff,FloorArea, ECM = ''):
+    #add to all zones an ideal load element driven by the above thermostat
+    #DCV stands for Demand Controlled Ventilation, the airflow is in m3/s/m2 thus divded by 1000 from yml
+    # try:
+    AreaBasedFlowRate = Correctdeff['AreaBasedFlowRate']
+    idf.newidfobject(
+        "HVACTEMPLATE:ZONE:IDEALLOADSAIRSYSTEM",
+        Zone_Name=zone.Name,
+        Template_Thermostat_Name=ThermostatName,
+        Heat_Recovery_Type='Sensible' if building.VentSyst['BalX'] or building.VentSyst['ExhX'] else 'None',
+        Sensible_Heat_Recovery_Effectiveness= Correctdeff['HReff']*building.AirRecovEff if building.VentSyst['BalX'] or building.VentSyst['ExhX'] else 0,
+        Latent_Heat_Recovery_Effectiveness= 0,
+        #Design_Specification_Outdoor_Air_Object_Name = AirNode,
+        Outdoor_Air_Method='Sum' if PeopleDensity>0 and building.DemandControlledVentilation else 'Flow/Area',
+        Outdoor_Air_Flow_Rate_per_Zone_Floor_Area=Multiplier*AreaBasedFlowRate/1000 + Multiplier*building.OccupBasedFlowRate/1000*PeopleDensity*(1-building.DemandControlledVentilation),
+        Outdoor_Air_Flow_Rate_per_Person=building.OccupBasedFlowRate/1000,
+        Demand_Controlled_Ventilation_Type = 'OccupancySchedule' if PeopleDensity>0 and building.DemandControlledVentilation else 'None',
+        Heating_Limit = building.HVACLimitMode,
+        Maximum_Sensible_Heating_Capacity =  FloorArea*building.HVACPowLimit, #the floor area already takes into account the zone multiplier
+        #Outdoor_Air_Inlet_Node_Name = 'OutdoorAirNode'
+        )
+    idf.newidfobject(
+        "OUTDOORAIR:NODE",
+        Name=zone.Name+' IDEAL LOADS OUTDOOR AIR INLET',  # quite weird but it seems to work....the name does not referer to the otherone,
+    )
+    return id
---
+++ bin/Enegy_Conservation_Measure/Set_Retrofit.py
@@ -0,0 +1,91 @@
+import os
+import sys
+sys.path.append('..')
+from bin.core import setConfig
+import bin.core.GeneralFunctions as GrlFct
+from bin.ECM import HVACSystems
+
+class ECM_prep:
+
+    def add_ECM(self, keypath, LogFile, CurrentBldID): #, LogFile, nbcase
+        ecm = ECM(keypath,LogFile, CurrentBldID, )
+        return ecm
+
+class ECM:
+    def __init__(self, ECM_Config_Path, LogFile, CurrentBldID = ''):
+
+        self.ecm_Config = setConfig.read_yaml('/Users/alizad/Desktop/MUBES/mubes-ubem-main/bin/ECM/ECM_Config.yml')
+        self.ecm_Config_Unit = setConfig.read_yaml('/Users/alizad/Desktop/MUBES/mubes-ubem-main/bin/ECM/ECM_ConfigKeyUnit.yml')
+        self.LogFile = LogFile
+        self.Retrofit = True
+        # self.ecmConfig = setConfig.read_yaml (ECM_Config_Path['ECM_Path']) #('../../bin/ECM/ECM_Config.yml')
+        self.Envelope_Case = self.ecm_Config['ECM_Category']['Envelope']
+        self.HVAC_Case = self.ecm_Config['ECM_Category']['Envelope']
+        self.CurrentBldID = CurrentBldID
+        self.MatchBldID = self.ecm_Config['RetCase']['BuildID']
+
+
+    def Check_Selected_ECM(self, mainConfig):
+        ecm_options = []
+        for idx, ecm in enumerate(mainConfig):
+            # if ecm in ['Envelop', 'HVAC']:
+            ecm_options.append(ecm)
+        if len(ecm_options) > 1:
+            msg = '[ECM info] ******* ' + f"{' and '.join(ecm_options)} will be considered as retrofitting options" + ' ******* \n'
+        else:
+            msg = '[ECM info] ******* ' + f"{ecm_options[0]} will be considered as retrofitting option" + ' ******* \n'
+        return  msg#, ecm_options
+
+    def CheckConfigUnit_ECM(self):
+        flag = setConfig.checkConfigUnit(self.ecm_Config, self.ecm_Config_Unit, path='../ECM/ECM_Config.yml')
+        if type(flag) != dict:
+            msg = '[Config Error] Something seems wrong : \n' + flag
+            print(msg)
+            GrlFct.Write2LogFile(flag, self.LogFile)
+            flag = True
+            # sys.exit()
+            return msg, flag
+        else:
+            flag = False
+            return ' ', flag
+
+
+    def Check_Bld_Existance(self, SimBldId, Ret_BldIds, currentBldID):
+        mismatch = []
+        match = []
+        if len(Ret_BldIds) > 0:
+            for mtch in Ret_BldIds:
+                if mtch in SimBldId:
+                    match.append(mtch)
+                else:
+                    mismatch.append(mtch)
+
+            if currentBldID in match:
+                msg = (f"[ECM info] Current building will be simulated with ECMs. In general {len(match)} selected buildings for retrofitting are also in simulation studies and "
+                   f"{len(mismatch)} of selected buildings are out of scope of the simulation \n")
+                self.isBuildID = True
+
+            else:
+                msg = (f"[ECM info] Current building wont be simulated with ECMs. In general {len(match)} selected buildings for retrofitting are also in simulation studies and "
+                       f"{len(mismatch)} of selected buildings are out of scope of the simulation \n")
+                self.isBuildID = False
+            return msg, self.isBuildID
+
+        else:
+            msg = f" ** [Error] Seems like you want to make retrofitting studies, but you have not specified any building in 'ECM_Config.yml' \n"
+            return msg, None
+
+
+    def UpdateMaterial(self, Material_Old, nbcase, LogFile):
+
+        Material_Old.update(self.ecm_Config['ECM_Category']['Envelope'])
+        return Material_Old
+
+
+    def HVAC_Measures(self, idf,zone,building,PeopleDensity,ThermostatName, Multiplier,Correctdeff,FloorArea):
+        HVACSystems.ZoneCtrl(idf,zone,building,PeopleDensity,ThermostatName, Multiplier,Correctdeff,FloorArea)
+        return 4
+    #
+    # # def Update_Construction():
+    # #
+    # def Check_Unit(EPCYML):
